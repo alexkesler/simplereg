7476dcb829f3b63ff55b1d067d6a5562bcd44a41
Alexey <kesler@mail.ru>
2013-07-08 09:08:23 +0600

    compiled with ReceptionsView

--- src/main/java/org/kesler/simplereg/gui/ReceptionView.java
+++ src/main/java/org/kesler/simplereg/gui/ReceptionView.java
@@ -1,8 +1,11 @@
 package org.kesler.simplereg.gui;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.*;
 
-import org.kesler.simplereg.Reception; 
+import org.kesler.simplereg.logic.Reception; 
 
 
 public class ReceptionView extends JFrame {
@@ -10,18 +13,36 @@ public class ReceptionView extends JFrame {
 	private MainViewController controller = null;
 
 	public ReceptionView(MainViewController controller) {
+		super("Новая регистрация");
 		this.controller = controller;
 		createGUI();
 	}
 
 	public ReceptionView(MainViewController controller, Reception reception) {
+		super("Редактирование регистрации");
 		this.controller = controller;
 		this.reception = reception;
 		createGUI();
 	}
 
 	private void createGUI() {
+		this.setSize(300,300);
+		JPanel mainPanel = new JPanel(new BorderLayout());
 
+		
+
+		JPanel buttonPanel = new JPanel();
+		JButton saveButton = new JButton("Сохранить");
+
+		JButton cancelButton = new JButton("Отмена");
+
+		buttonPanel.add(saveButton);
+		buttonPanel.add(cancelButton);
+
+		mainPanel.add(BorderLayout.SOUTH, buttonPanel);
+
+		this.add(mainPanel, BorderLayout.CENTER);
+		this.pack();
 	}
 
 }
