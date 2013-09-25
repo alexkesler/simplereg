2640fe9c3438abdbf5bbdafc0abf34dca72b86b3
Alexey <kesler@mail.ru>
2013-09-25 17:59:14 +0600

    Continuing reestrView

---
+++ src/main/java/org/kesler/simplereg/gui/reestr/ReestrFilterDialog.java
@@ -0,0 +1,21 @@
+package org.kesler.simplereg.gui.reestr;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+public class ReestrFilterDialog extends JDialog {
+
+	private JFrame frame;
+
+	ReestrFilterDialog(JFrame frame) {
+		super(frame, true);
+		this.frame = frame;
+
+		createGUI();
+	}
+
+	private void createGUI() {
+		
+	}
+
+}
--- src/main/java/org/kesler/simplereg/gui/reestr/ReestrView.java
+++ src/main/java/org/kesler/simplereg/gui/reestr/ReestrView.java
@@ -2,11 +2,16 @@ package org.kesler.simplereg.gui.reestr;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
-
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.awt.BorderLayout;
+
+import net.miginfocom.swing.MigLayout;
 
 import org.kesler.simplereg.logic.Reception;
 
@@ -17,6 +22,8 @@ public class ReestrView extends JFrame {
 
 	private JTable reestrTable;
 	private ReestrTableModel reestrTableModel;
+
+	private JLabel filterLabel;
 
 	public ReestrView(ReestrViewController controller) {
 		super("Реестр запросов");
@@ -30,21 +37,37 @@ public class ReestrView extends JFrame {
 	private void createGUI() {
 		JPanel mainPanel = new JPanel(new BorderLayout());
 
-		JPanel dataPanel = new JPanel();
+		JPanel filterPanel = new JPanel(new MigLayout("fill, nogrid"));
+
+		filterLabel = new JLabel();
+		JButton setFilterButton = new JButton("Изменить");
+
+		filterPanel.add(new JLabel("Фильтр: "));
+		filterPanel.add(filterLabel, "growx");
+		filterPanel.add(setFilterButton);
+
+		JPanel dataPanel = new JPanel(new MigLayout("fill, nogrid"));
 
 
 		reestrTableModel = new ReestrTableModel();
 		reestrTable = new JTable(reestrTableModel);
+		JScrollPane reestrTableScrollPane = new JScrollPane(reestrTable);
 
-		dataPanel.add(reestrTable);
+		dataPanel.add(reestrTableScrollPane, "push, grow");
 
 
 		JPanel buttonPanel = new JPanel();
 
 		JButton okButton = new JButton("Ok");
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				setVisible(false);
+			}
+		});
 
 		buttonPanel.add(okButton);
 
+		mainPanel.add(filterPanel, BorderLayout.NORTH);
 		mainPanel.add(dataPanel, BorderLayout.CENTER);
 		mainPanel.add(buttonPanel, BorderLayout.SOUTH);
 
