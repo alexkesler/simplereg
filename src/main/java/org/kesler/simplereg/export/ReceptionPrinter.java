c6ba4160eb53f5e0fb173c7bd42c7c72fb4a4de4
Alexey <kesler@mail.ru>
2013-12-04 17:49:35 +0600

    some improvements

--- src/main/java/org/kesler/simplereg/export/ReceptionPrinter.java
+++ src/main/java/org/kesler/simplereg/export/ReceptionPrinter.java
@@ -1,7 +1,19 @@
 package org.kesler.simplereg.export;
 
+import org.kesler.simplereg.logic.reception.Reception;
 
+public abstract class ReceptionPrinter {
 
-public class ReceptionPrinter {
+	protected Reception reception;
+
+	ReceptionPrinter(Reception reception) {
+		this.reception = reception;
+	}
+
+	public abstract void printReception();
+
+	protected void saveFile() {
+		
+	}
 
 }
---
+++ src/main/java/org/kesler/simplereg/export/RosReestrReceptionPrinter.java
@@ -0,0 +1,20 @@
+package org.kesler.simplereg.export;
+
+import org.kesler.simplereg.logic.reception.Reception;
+
+public class RosReestrReceptionPrinter extends ReceptionPrinter {
+
+	RosReestrReceptionPrinter(Reception reception) {
+		super(reception);
+
+	}
+
+
+	@Override
+	public void printReception() {
+
+	}
+
+
+
+}
