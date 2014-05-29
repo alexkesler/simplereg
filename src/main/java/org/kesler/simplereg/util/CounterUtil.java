62e530b00816edd86c32a06d2dd35514bcb3c6cb
Alexey <kesler@mail.ru>
2014-05-29 07:45:07 +0600

    fix create new counter

--- src/main/java/org/kesler/simplereg/util/CounterUtil.java
+++ src/main/java/org/kesler/simplereg/util/CounterUtil.java
@@ -4,6 +4,8 @@ import org.apache.log4j.Logger;
 
 import org.kesler.simplereg.dao.DAOFactory;
 
+import java.util.List;
+
 
 public class CounterUtil {
 	private static final Logger log = Logger.getLogger("CounterUtil");
@@ -12,23 +14,31 @@ public class CounterUtil {
 		int count = 1;
 
 		log.info("Getting current count");
-		Counter currentCount = DAOFactory.getInstance().getCounterDAO().getItemById(1L);
-		if (currentCount == null) {
-			log.warn("Current count is null - creating new");
-			currentCount = new Counter();
-			currentCount.setId(1L);
-			currentCount.setValue(count);
-			DAOFactory.getInstance().getCounterDAO().addItem(currentCount);
-		} else {
-			count = currentCount.getValue();
-			log.info("Current count: " + count);
-			count++;
-			currentCount.setValue(count);
-			log.info("Saving new count: " + count);
-			DAOFactory.getInstance().getCounterDAO().updateItem(currentCount);
-		}
+
+        List<Counter> counters = DAOFactory.getInstance().getCounterDAO().getAllItems();
+        if(counters.size()==0) { createCounter(); return count;}
+
+        Counter currentCount = null;
+        currentCount = DAOFactory.getInstance().getCounterDAO().getItemById(1L);
+        if (currentCount == null) { createCounter(); return count;}
+
+        count = currentCount.getValue();
+        log.info("Current count: " + count);
+        count++;
+        currentCount.setValue(count);
+        log.info("Saving new count: " + count);
+        DAOFactory.getInstance().getCounterDAO().updateItem(currentCount);
 
 		return count;
 	}
+
+    private static void createCounter() {
+        log.warn("Current count is absent - creating new");
+        Counter currentCount = new Counter();
+        currentCount.setId(1L);
+        currentCount.setValue(1);
+        DAOFactory.getInstance().getCounterDAO().addItem(currentCount);
+
+    }
 
 }
