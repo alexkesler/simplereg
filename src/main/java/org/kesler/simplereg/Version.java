955558a79d73ee448d457eb46ab02eb8adb08321
Alexey <kesler@mail.ru>
2014-09-11 11:36:47 +0600

    Bumped version number to 1.3.3

--- src/main/java/org/kesler/simplereg/Version.java
+++ src/main/java/org/kesler/simplereg/Version.java
@@ -5,7 +5,7 @@ package org.kesler.simplereg;
  */
 public class Version {
 
-    private static String version = "1.3.1";
+    private static String version = "1.3.3";
     private static String releaseDate = "11.09.2014";
 
     public static String getVersion() {
