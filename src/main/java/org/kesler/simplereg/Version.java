a1ed611efa30bf3e79a3b4a54b8a943f7f29590c
Alexey <kesler@mail.ru>
2014-09-11 10:48:02 +0600

    Bump build date

--- src/main/java/org/kesler/simplereg/Version.java
+++ src/main/java/org/kesler/simplereg/Version.java
@@ -6,7 +6,7 @@ package org.kesler.simplereg;
 public class Version {
 
     private static String version = "1.3.1";
-    private static String releaseDate = "10.09.2014";
+    private static String releaseDate = "11.09.2014";
 
     public static String getVersion() {
         return version;
