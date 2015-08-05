0a6fdb42d559ea6a47ace4a466c4fae93d2b418e
Alexey <kesler@mail.ru>
2015-08-05 16:07:58 +0500

    Little fix

--- src/main/java/org/kesler/simplereg/logic/reception/filter/ReceptionsFilter.java
+++ src/main/java/org/kesler/simplereg/logic/reception/filter/ReceptionsFilter.java
@@ -3,8 +3,8 @@ package org.kesler.simplereg.logic.reception.filter;
 import org.kesler.simplereg.logic.Reception;
 
 public interface ReceptionsFilter {
-    public ReceptionsFiltersEnum getFiltersEnum();
-	public boolean checkReception(Reception reception);
+	ReceptionsFiltersEnum getFiltersEnum();
+	boolean checkReception(Reception reception);
 	@Override
-	public String toString();
+	String toString();
 }
